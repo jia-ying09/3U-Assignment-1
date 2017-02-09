6f1522500751b33a65b864f53cb8d95aeabbd796
jiaying <jiaying@172.21.15.73>
2017-02-09 10:45:20 -0500

    initial commit of Q2A2

--- src/assignment1/A1Q2.java
+++ src/assignment1/A1Q2.java
@@ -4,23 +4,59 @@
  */
 package assignment1;
 
+
 /**
  *
  * @author linj4653
  */
+import becker.robots.Direction;
+import becker.robots.Robot;
+import becker.robots.Thing;
+import becker.robots.Wall;
+import becker.robots.City;
 public class A1Q2 {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // New city
-        City kitchener = new City
-        //create new robot
-        Robot timmy = new Robot(kitchener, 3, 3, Direction.East)
-        //creating newspaper
-        new Thing(kitchener.)
-                
+        // creating kitchener city
+        City kitchener = new City();
+        //creating robot timmy
+        Robot timmy = new Robot(kitchener, 1, 3, Direction.NORTH);
+        //creating square wall
+        new Wall (kitchener, 1, 3, Direction.NORTH);
+        new Wall (kitchener, 1, 2, Direction.NORTH);
+        new Wall (kitchener, 1, 2, Direction.WEST);
+        new Wall (kitchener, 2, 2, Direction.WEST);
+        new Wall (kitchener, 2, 2, Direction.SOUTH);
+        new Wall (kitchener, 1, 3, Direction.EAST);
+        new Wall (kitchener, 1, 3, Direction.SOUTH);
+        //inserting newspaper
+        new Thing (kitchener, 2, 3);
+        //making timmy move around the square wall
+        timmy.turnLeft();
+        timmy.move();
+        timmy.turnLeft();    
+        timmy.move();      
+        timmy.turnLeft();
+        timmy.move();
+         //timmy picks up the newspaper
+        timmy.pickThing();
+        timmy.turnLeft();
+        timmy.turnLeft();
+        timmy.move();
+        timmy.turnLeft();
+        timmy.turnLeft();
+        timmy.turnLeft();
+        timmy.move();
+        timmy.turnLeft();
+        timmy.turnLeft();
+        timmy.turnLeft();
+        timmy.move();
+        timmy.turnLeft();
+        timmy.turnLeft();
+        timmy.turnLeft();
         
         
     }
