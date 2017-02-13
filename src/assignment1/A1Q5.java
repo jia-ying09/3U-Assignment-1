9d38ae43d91ab2398eef2ab915b1892fbffb6124
jiaying <jiaying@172.21.15.73>
2017-02-13 18:49:29 -0500

    initial commit of Assignment

---
+++ src/assignment1/A1Q5.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package assignment1;
+
+import becker.robots.City;
+import becker.robots.Direction;
+import becker.robots.Robot;
+import becker.robots.Thing;
+import becker.robots.Wall;
+
+/**
+ *
+ * @author jiaying
+ */
+public class A1Q5 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // making Kitchener city
+        City kitchener = new City();
+        //creating timmy
+        Robot timmy = new Robot(kitchener, 3, 3, Direction.EAST);
+        //creating maria
+        Robot maria = new Robot(kitchener, 0, 1, Direction.WEST);
+        //creating items
+        new Thing(kitchener, 0, 0);
+        new Thing(kitchener, 1, 0);
+        new Thing(kitchener, 1, 1);
+        new Thing(kitchener, 1, 2);
+        new Thing(kitchener, 2, 2);
+        //creating supernmarket
+        new Wall(kitchener, 2, 3, Direction.WEST);
+        new Wall(kitchener, 2, 3, Direction.NORTH);
+        new Wall(kitchener, 2, 3, Direction.EAST);
+        new Wall(kitchener, 3, 3, Direction.EAST);
+        new Wall(kitchener, 3, 3, Direction.SOUTH);
+        //MAKING THE ROBOTS MOVE
+        timmy.turnLeft();
+        maria.move();
+        maria.pickThing();
+        maria.turnLeft();               
+        timmy.turnLeft();
+        timmy.move();
+        maria.move();
+        timmy.turnLeft();
+        timmy.turnLeft();
+        timmy.turnLeft();
+        maria.turnLeft();
+        maria.pickThing();
+        timmy.move();
+        timmy.pickThing();
+        timmy.move();
+        maria.move();       
+        timmy.pickThing();
+        timmy.turnLeft(); maria.pickThing();
+        
+        
+        
+        
+        
+    }
+    
+}
